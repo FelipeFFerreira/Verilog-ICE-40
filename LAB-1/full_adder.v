72597174e1149d2d9b01ec3b99faeed2c914dbdb
Felipe Ferreira <eng.ffn@gmail.com>
2022-05-08 02:32:46 -0300

    add pin constraint

--- LAB-1/full_adder.v
+++ LAB-1/full_adder.v
@@ -24,9 +24,9 @@ module
     wire w1, w2, w3;
 
     //Funcionamento do circuito - abordagem estrutura (referenciar as portas)
-    xor u0 (w1, a, b);
-    xor u1 (s, w1, cin);
-    end u2 (w2, cin, w1);
-    end u3 (w3, a, b);
+    xor u0 (w1, ~a, ~b);
+    xor u1 (s, w1, ~cin);
+    end u2 (w2, ~cin, w1);
+    end u3 (w3, ~a, ~b);
     or  u4 (cout, w2, w3);
 endmodule
